fa7177c8f360132355eb6b81c63e4462433cab12
Happy-potamus <134818253+Happy-potamus@users.noreply.github.com>
2026-02-03 23:32:57 -0800

    Add files via upload

---
+++ data/bruinCafeData.ts
@@ -0,0 +1,33 @@
+import { Food } from "../models/Food";
+
+export const bruin_sandwiches: Food[] = [
+  new Food("Ham & Swiss Sandwich", 0, 0),
+  new Food("Roasted Turkey & Provolone Sandwich", 0, 0),
+  new Food("Smoky BBQ Sandwich", 0, 0),
+  new Food("BBQ Beef Brisket Sandwich", 0, 0),
+  new Food("Chicken Ranch & Swiss Sandwich", 0, 0),
+  new Food("The Cuban Sandwich", 0, 0),
+  new Food("Chipotle BBQ Turkey Sandwich", 0, 0),
+  new Food("Chicken Caesar Sandwich", 0, 0),
+  new Food("Roasted Salmon Sandwich", 0, 0),
+  new Food("Pastrami & Swiss Sandwich", 0, 0),
+  new Food("Chicken Pesto Sandwich", 0, 0),
+  new Food("Roasted Portobello Mushroom Sandwich", 0, 0),
+  new Food("Buffalo Chicken Sandwich", 0, 0),
+  new Food("Cheesesteak", 0, 0),
+  new Food("Vegan Chicken Caesar Sandwich", 0, 0),
+  new Food("Chipotle BBQ Sandwich", 0, 0),
+  new Food("Havana Press Sandwich", 0, 0),
+  new Food("Roasted Garden Salmon Sandwich", 0, 0),
+  new Food("Vegetarian Chicken Ranch & Swiss Sandwich", 0, 0),
+  new Food("Vegetarian Pastrami & Swiss Sandwich", 0, 0),
+  new Food("Vegetarian Ham & Swiss Sandwich", 0, 0),
+];
+
+export const bruin_sides: Food[] = [
+  new Food("Cookies", 0, 0),
+  new Food("Chips", 0, 0),
+  new Food("Banana", 0, 0),
+  new Food("Apple", 0, 0),
+  new Food("Orange", 0, 0),
+];
---
+++ data/hedrickStudyData.ts
@@ -0,0 +1,35 @@
+import { Food } from "../models/Food";
+
+export const hedrick_bread: Food[] = [
+  new Food("Rye", 0, 0),
+  new Food("Italian", 0, 0),
+  new Food("Sourdough", 0, 0),
+  new Food("Whole Wheat", 0, 0),
+];
+
+export const hedrick_protein: Food[] = [
+  new Food("None", 0, 0),
+  new Food("Chicken", 0, 0),
+  new Food("Pepperoni", 0, 0),
+  new Food("Roast Beef", 0, 0),
+  new Food("Turkey", 0, 0),
+  new Food("Ham", 0, 0),
+  new Food("Bacon", 0, 0),
+  new Food("Avocado", 0, 0),
+];
+
+export const hedrick_cheese: Food[] = [
+  new Food("None", 0, 0),
+  new Food("Vegan Cheddar", 0, 0),
+  new Food("Mozzarella", 0, 0),
+  new Food("Provolone", 0, 0),
+  new Food("Cheddar", 0, 0),
+];
+
+export const hedrick_sides: Food[] = [
+  new Food("Cookies", 0, 0),
+  new Food("Chips", 0, 0),
+  new Food("Banana", 0, 0),
+  new Food("Apple", 0, 0),
+  new Food("Orange", 0, 0),
+];
